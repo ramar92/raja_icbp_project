4f3ddb0320197da5b7f408e7e144aaf1628c177a
RAMAR BOSE <83829394+ramar92@users.noreply.github.com>
2024-12-07 17:47:33 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,8 @@
+import pickle
+import pandas as pd
+import streamlit as st
+from streamlit_option_menu import option_menu
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from PIL import Image
